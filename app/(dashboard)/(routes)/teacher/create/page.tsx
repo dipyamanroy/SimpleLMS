668dfbebb95468940c29014ccf2371e9711f113a
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-12 14:34:39 +0530

    Added Prisma and APIs for course creation.

--- app/(dashboard)/(routes)/teacher/create/page.tsx
+++ app/(dashboard)/(routes)/teacher/create/page.tsx
@@ -39,8 +39,9 @@ const CreatePage = () => {
 
     const onSubmit = async (values: z.infer<typeof formSchema>) => {
         try{
-            const response = await axios.post("/api/course", values);
-            router.push(`/teacher/course/${response.data.id}`);
+            const response = await axios.post("/api/courses", values);
+            router.push(`/teacher/courses/${response.data.id}`);
+            toast.success("Course created");
         } catch {
             toast.error("Something went wrong")
         }
