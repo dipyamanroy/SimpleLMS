d78212c0bff16d3fe1c8ad533623585367acc6bb
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-18 00:22:47 +0530

    Fixed bug in combobox element.

--- components/ui/combobox.tsx
+++ components/ui/combobox.tsx
@@ -37,7 +37,7 @@ export const Combobox = ({
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-full justify-between"
         >
           {value
             ? options.find((option) => option.value === value)?.label
@@ -45,7 +45,7 @@ export const Combobox = ({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full p-0">
         <Command>
           <CommandInput placeholder="Search option..." />
           <CommandEmpty>No option found.</CommandEmpty>
