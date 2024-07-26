aa8cf09ec0584e317ad422e929ba5d6656c4583b
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-26 14:43:48 +0530

    Added branding.

--- app/(dashboard)/_components/logo.tsx
+++ app/(dashboard)/_components/logo.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export const Logo = () => {
     return (
         <Image 
-            height={130}
-            width={130}
+            height={155}
+            width={155}
             alt="logo"
             src="/logo.svg"
         />
