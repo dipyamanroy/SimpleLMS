def7f6bfb92318a742c3a7778b026734266e9d1d
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-26 16:20:49 +0530

    Added chapter creation API.

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -10,6 +10,7 @@ import ImageForm from "./_components/image-form";
 import CategoryForm from "./_components/category-form";
 import PriceForm from "./_components/price-form";
 import AttachmentForm from "./_components/attachment-form";
+import ChaptersForm from "./_components/chapters-form";
 
 const CourseIdPage = async ({
     params
@@ -23,8 +24,16 @@ const CourseIdPage = async ({
     }
 
     const course = await db.course.findUnique({
-        where: { id: params.courseId },
+        where: { 
+            id: params.courseId,
+            userId
+         },
         include: {
+            chapters: {
+                orderBy: {
+                    position: "asc",
+                },
+            },
             attachments: {
                 orderBy: {
                     createdAt: "desc",
@@ -33,23 +42,24 @@ const CourseIdPage = async ({
         },
     });
 
+    if (!course) {
+        return redirect("/");
+    }
+
     const categories = await db.category.findMany({
         orderBy: { 
             name: "asc",
         },
     
     });
-    
-    if (!course) {
-        return redirect("/");
-    }
 
     const requiredFields = [
         course.title,
         course.description,
         course.imageUrl,
         course.price,
-        course.categoryId
+        course.categoryId,
+        course.chapters.some(chapter => chapter.isPublished),
     ];
 
     const totalFields = requiredFields.length;
@@ -106,9 +116,10 @@ const CourseIdPage = async ({
                                 Course chapters
                             </h2>
                         </div>
-                        <div>
-                            TODO: Chapters
-                        </div>
+                        <ChaptersForm
+                            initialData={course}
+                            courseId={course.id}
+                        />
                     </div>
                     <div>
                         <div className="flex items-center gap-x-2">
