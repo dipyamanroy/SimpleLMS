99643d1a75cc3ab349b37bd951411f9ced137741
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-29 16:34:52 +0530

    Added chapter description functionality.

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -6,6 +6,7 @@ import Link from "next/link";
 import { ArrowLeft, LayoutDashboard } from "lucide-react";
 import { IconBadge } from "@/components/icon-badge";
 import ChapterTitleForm from "./_components/chapter-title-form";
+import ChapterDescriptionForm from "./_components/chapter-description-form";
 
 const ChapterIdPage = async ({
     params
@@ -77,6 +78,11 @@ const ChapterIdPage = async ({
                             </h2>
                         </div>
                         <ChapterTitleForm
+                            initialData={chapter}
+                            courseId={params.courseId}
+                            chapterId={params.chapterId}
+                        />
+                        <ChapterDescriptionForm
                             initialData={chapter}
                             courseId={params.courseId}
                             chapterId={params.chapterId}
---
+++ components/preview.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
+
+import "react-quill/dist/quill.bubble.css";
+
+interface PreviewProps {
+    value: string;
+};
+
+export const Preview = ({
+    value,
+}: PreviewProps) => {
+    const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), {ssr: false}), [])
+
+    return (
+            <ReactQuill
+                theme="bubble"
+                value={value}
+                placeholder="The Force is a mystical energy field..."
+                readOnly
+            />
+    );
+};
