6d66d1198e9c248413eae60881cf271ca4b33bc1
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-30 16:57:00 +0530

    Added reusable confirm modal.

---
+++ components/modals/confirm-modal.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+
+interface ConfirmModalProps {
+    children: React.ReactNode;
+    onConfirm: () => void;
+};
+
+export const ConfirmModal = ({
+    children,
+    onConfirm,
+}: ConfirmModalProps) => {
+    return (
+        <AlertDialog>
+            <AlertDialogTrigger asChild>
+                {children}
+            </AlertDialogTrigger>
+            <AlertDialogContent>
+                <AlertDialogHeader>
+                    <AlertDialogTrigger>Are you sure?</AlertDialogTrigger>
+                    <AlertDialogDescription>
+                        This action cannot be undone.
+                    </AlertDialogDescription>
+                </AlertDialogHeader>
+                <AlertDialogFooter>
+                    <AlertDialogCancel>
+                        Cancel
+                    </AlertDialogCancel>
+                    <AlertDialogAction onClick={onConfirm}>
+                        Continue
+                    </AlertDialogAction>
+                </AlertDialogFooter>
+            </AlertDialogContent>
+        </AlertDialog>
+    )
+}
