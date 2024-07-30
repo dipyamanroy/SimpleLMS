0cef7cfe7d0affbac41c07d834a5347c0e6bfe12
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-30 17:17:56 +0530

    Added Chapter deletion API route.

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import axios from "axios";
+import { useState } from "react";
 import { Trash } from "lucide-react";
-
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 import { Button } from "@/components/ui/button";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+
 
 interface ChapterActionsProps {
     disabled: boolean;
@@ -18,19 +23,40 @@ export const ChapterActions = ({
     chapterId,
     isPublished,
 }: ChapterActionsProps) => {
+    const router = useRouter();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const onDelete = async () => {
+        try{
+            setIsLoading(true);
+
+            await axios.delete(`/api/courses/${courseId}/chapter/${chapterId}`);
+
+            toast.success("Chapter deleted");
+            router.refresh();
+            router.push(`/teacher/courses/${courseId}`)
+        } catch {
+            toast.error("Something went wrong")
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
     return (
         <div className="flex items-center gap-x-2">
             <Button
                 onClick={() => {}}
-                disabled={disabled}
+                disabled={disabled || isLoading}
                 variant="outline"
                 size="sm"
             >
                 {isPublished ? "Unpublish" : "Publish"}
             </Button>
-            <Button size="sm">
-                <Trash className="h-4 w-4"/>
-            </Button>
+            <ConfirmModal onConfirm={onDelete}>
+                <Button size="sm" disabled={isLoading}>
+                    <Trash className="h-4 w-4"/>
+                </Button>
+            </ConfirmModal>
         </div>
     )
 }
