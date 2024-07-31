8a1f68aa775b176e56dcedd15b5808d4bc068d8c
dipyamanroy <d1pyamanroy@protonmail.com>
2024-07-31 15:53:12 +0530

    Fixed font rendering issue.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ToastProvider } from "@/components/providers/toaster-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
