58553da5ed96a90054b05732be02934728b1ba09
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-02 17:44:47 +0530

    Added column and data table elements.

---
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Course } from "@prisma/client"
+import { ColumnDef } from "@tanstack/react-table"
+
+export const columns: ColumnDef<Course>[] = [
+    {
+        accessorKey: "title",
+        header: "Title",
+    },
+    {
+        accessorKey: "price",
+        header: "Price",
+    },
+    {
+        accessorKey: "amount",
+        header: "Amount",
+    },
+]
--- app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,14 +1,26 @@
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
 
-const CoursesPage = () => {
+async function getData(): Promise<any[]> {
+    // Fetch data from your API here.
+    return [
+        {
+        id: "728ed52f",
+        amount: 100,
+        status: "pending",
+        email: "m@example.com",
+        },
+      // ...
+    ]
+}
+
+const CoursesPage = async () => {
+
+    const data = await getData();
+    
     return (
         <div className="p-6">
-            <Link href="/teacher/create">
-                <Button>
-                    New Course
-                </Button>
-            </Link>
+            <DataTable columns={columns} data={data} />
         </div>
     );
 }
