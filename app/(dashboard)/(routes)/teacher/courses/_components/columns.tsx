3dfa22cbc2cb0f98633c485badc9d46cdd0587df
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-03 16:04:49 +0530

    Added course actions to courses data-table.

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -1,9 +1,17 @@
 "use client"
 
+import Link from "next/link";
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
-import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { ArrowUpDown, MoreHorizontal, Pencil } from "lucide-react"
+
 import { Button } from "@/components/ui/button";
+import { 
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 
 export const columns: ColumnDef<Course>[] = [
     {
@@ -47,5 +55,30 @@ export const columns: ColumnDef<Course>[] = [
                 </Button>
             )
         },
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const { id } = row.original;
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-4 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4 ml-2" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <Link href={`/teacher/courses/${id}`}>
+                            <DropdownMenuItem>
+                                <Pencil className="h-4 w-4 mr-2"/>
+                                Edit
+                            </DropdownMenuItem>
+                        </Link>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        }
     },
 ]
