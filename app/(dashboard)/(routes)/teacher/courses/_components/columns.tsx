6ff4e5096d0b4aae1869b45227a51c37d20b2239
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-03 15:51:09 +0530

    Added sorting by title in courses landing page.

--- app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
+++ app/(dashboard)/(routes)/teacher/courses/_components/columns.tsx
@@ -2,11 +2,23 @@
 
 import { Course } from "@prisma/client"
 import { ColumnDef } from "@tanstack/react-table"
+import { ArrowUpDown, MoreHorizontal } from "lucide-react"
+import { Button } from "@/components/ui/button";
 
 export const columns: ColumnDef<Course>[] = [
     {
         accessorKey: "title",
-        header: "Title",
+        header: ({ column }) => {
+            return(
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Title
+                    <ArrowUpDown className="ml-2 h-4 w-4"/>
+                </Button>
+            )
+        },
     },
     {
         accessorKey: "price",
