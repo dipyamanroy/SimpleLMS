fd8186a5175122021d4d6636dfb48cfab01eed31
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-11 21:32:27 +0530

    Added category form.

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,9 +1,21 @@
-const SearchPage = () => {
+import { db } from "@/lib/db";
+
+import { Categories } from "./_components/categories";
+
+const SearchPage = async () => {
+    const category = await db.category.findMany({
+        orderBy: {
+            name: "asc"
+        },
+    })
+    
     return ( 
-        <div>
-            This is a search page!
+        <div className="p-6">
+            <Categories 
+                items = {categories}
+            />
         </div>
     );
 }
- 
+
 export default SearchPage;
