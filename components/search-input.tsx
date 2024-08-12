5ef316477795088438a3267527143538bc557f45
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-12 21:19:51 +0530

    Added search input element to the search page.

--- components/search-input.tsx
+++ components/search-input.tsx
@@ -1,10 +1,20 @@
 "use client"
 
+import { Search } from "lucide-react"
+
+import { Input } from "@/components/ui/input"
+
 export const SearchInput = () => {
     
     return (
-        <div>
-            Search!
+        <div className="relative">
+            <Search
+                className="h-4 w-4 absolute top-3 left-3 text-slate-600"
+            />
+            <Input 
+                className="w-4 md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+                placeholder="Search for a course"
+            />
         </div>
     )
 }
