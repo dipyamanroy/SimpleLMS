39d34882db840148472645163023d4986c93a391
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-12 21:37:31 +0530

    Modified searchbar to render properly on mobile devices.

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db";
 
 import { Categories } from "./_components/categories";
+import { SearchInput } from "@/components/search-input";
 
 const SearchPage = async () => {
     const categories = await db.category.findMany({
@@ -10,11 +11,16 @@ const SearchPage = async () => {
     })
     
     return ( 
-        <div className="p-6">
-            <Categories 
-                items = {categories}
-            />
-        </div>
+        <>
+            <div className="px-6 pt-6 md:hidden md:mb-0 block">
+                <SearchInput />
+            </div>
+            <div className="p-6">
+                <Categories 
+                    items = {categories}
+                />
+            </div>
+        </>
     );
 }
 
--- components/search-input.tsx
+++ components/search-input.tsx
@@ -40,7 +40,7 @@ export const SearchInput = () => {
             <Input 
                 onChange={(e) => setValue(e.target.value)}
                 value={value}
-                className="w-4 md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+                className="w-full md:w-[300px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
                 placeholder="Search for a course"
             />
         </div>
