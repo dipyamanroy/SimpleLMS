ca83e2458ed35b43c9ee0dbb0d2792028b404823
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-13 13:35:19 +0530

    Added search page props.

--- app/(dashboard)/(routes)/search/page.tsx
+++ app/(dashboard)/(routes)/search/page.tsx
@@ -1,15 +1,39 @@
-import { db } from "@/lib/db";
 
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+import { db } from "@/lib/db";
 import { Categories } from "./_components/categories";
 import { SearchInput } from "@/components/search-input";
+import { getCourses } from "@/actions/get-courses";
 
-const SearchPage = async () => {
+interface SearchPageProps {
+    searchParams: {
+        title: string;
+        categoryId: string;
+    }
+}
+
+const SearchPage = async ({
+    searchParams
+}: SearchPageProps) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
     const categories = await db.category.findMany({
         orderBy: {
             name: "asc"
         },
     })
     
+    const courses = await getCourses({
+        userId,
+        ...searchParams,
+    });
+
     return ( 
         <>
             <div className="px-6 pt-6 md:hidden md:mb-0 block">
