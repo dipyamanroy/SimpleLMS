2476d0aad2bc16e27950cecdec8ac2a9da3bcad6
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-13 18:40:29 +0530

    Added course-sidebar-item component.

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -1,9 +1,46 @@
-"use client" 
+"use client"
 
-export const CourseSidebarItem = () => {
+import { CheckCircle, Lock, PlayCircle } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+import { cn } from "@/lib/utils";
+
+interface CourseSidebarItemProps {
+    label: string;
+    id: string;
+    isCompleted: boolean;
+    courseId: string;
+    isLocked: boolean;
+}
+
+export const CourseSidebarItem = ({
+    label,
+    id,
+    isCompleted,
+    courseId,
+    isLocked,
+}:CourseSidebarItemProps) => {
+    const pathname = usePathname();
+    const router = useRouter();
+
+    const Icon = isLocked ? Lock : (isCompleted ? CheckCircle : PlayCircle);
+    const isActive = pathname?.includes(id);
+
+    const onClick = () => {
+        router.push(`/courses/${courseId}/chapters/${id}`);
+    }
+
     return (
-        <div>
-            Course Sidebar Item
-        </div>
+        <button
+            type="button"
+            className={cn(
+
+            )}
+        >
+            <div>
+                <Icon />
+                {label}
+            </div>
+        </button>
     )
 }
