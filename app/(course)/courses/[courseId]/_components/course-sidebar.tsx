dfb70a452833c6baedc3b851a3b6af8ffd151153
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-13 18:31:18 +0530

    Added boilerplate for course-sidebar-item component.

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -17,7 +17,7 @@ export const CourseSidebar = async ({
     course,
     progressCount,
 }: CourseSidebarProps) => {
-    const userId = auth();
+    const { userId } = auth();
 
     if (!userId) {
         return redirect("/");
@@ -25,13 +25,33 @@ export const CourseSidebar = async ({
 
     const purchase = await db.purchase.findUnique({
         where: {
-            
+            userId_courseId: {
+                userId,
+                courseId: course.id, 
+            }
         }
-    })
+    });
 
     return (
-        <div>
-            Course sidebar
+        <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
+            <div className="p-8 flex flex-col border-b">
+                <h1 className="font-semibold">
+                    {course.title}
+                </h1>
+                {/* Check purchase and add progress */}
+            </div>
+            <div className="flex flex-col w-full">
+                {course.chapters.map((chapter) => (
+                    <CourseSidebarItem 
+                        key={chapter.id}
+                        id={chapter.id}
+                        label={chapter.title}
+                        isCompleted={!!chapter.userProgress?.[0]?.isCompleted}
+                        courseId={course.id}
+                        isLocked={!chapter.isFree && !purchase}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
