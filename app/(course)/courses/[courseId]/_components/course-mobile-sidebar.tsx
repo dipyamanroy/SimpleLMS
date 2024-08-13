930c3ad3b706ffbfb75847dcbd854b45752b10d2
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-13 19:03:06 +0530

    Added course-mobile-sidebar boilerplate.

---
+++ app/(course)/courses/[courseId]/_components/course-mobile-sidebar.tsx
@@ -0,0 +1,10 @@
+import { Menu } from "lucide-react"
+import { Chapter, Course, UserProgress } from "@prisma/client" 
+
+import {
+    Sheet,
+    SheetContent,
+    SheetTrigger
+} from "@/components/ui/sheet";
+
+import { CourseSidebar } from "./course-sidebar";
--- app/(course)/courses/[courseId]/_components/course-navbar.tsx
+++ app/(course)/courses/[courseId]/_components/course-navbar.tsx
@@ -17,6 +17,10 @@ export const CourseNavbar = ({
 }: CourseNavbarProps) => {
     return (
         <div className="p-4 border-b h-full flex items-center bg-white shadow-sm">
+            <CourseMobileSidebar 
+                course={course}
+                progressCount={progressCount}
+            />
             <NavbarRoutes />
         </div>
     )
