6354928016a7dade29fe1bf45081fb4b01f62e69
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-13 18:50:15 +0530

    Added placeholder for course-navbar in course layout.

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -49,13 +49,19 @@ const CourseLayout = async ({
 
     return (
         <div className="h-full">
+            <div className="h-[80] md:pl-80 fixed inset-y-0 w-full z-50">
+                <CourseNavbar 
+                    course={course}
+                    progressCount={progressCount}
+                />
+            </div>
             <div className="hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50">
                 <CourseSidebar
                     course={course}
                     progressCount={progressCount}
                 />
             </div>
-            <main className="md:pl-80 h-full">
+            <main className="md:pl-80 pt-[80] h-full">
                 {children}
             </main>
         </div>
