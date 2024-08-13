b9fa2b81331c924a82b7abc62d07114b73a8de8d
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-13 18:47:21 +0530

    Added styling to course-sidebar-item.

--- app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar-item.tsx
@@ -32,15 +32,31 @@ export const CourseSidebarItem = ({
 
     return (
         <button
+            onClick={onClick}
             type="button"
             className={cn(
-
+                "flex items-center gap-x-2 text-slate-500 text-sm font-[500] pl-6 transition-all hover:text-slate-600 hover:bg-slate-300/20",
+                isActive && "text-slate-700 bg-slate-200/20 hover:bg-slate-200/20 hover:text-slate-700",
+                isCompleted && "text-emerald-700 hover:text-emerald-700",
+                isCompleted && isActive && "bg-emerald-200/20",
             )}
         >
-            <div>
-                <Icon />
+            <div className="flex items-center gap-x-2 py-4">
+                <Icon 
+                    size={22}
+                    className={cn(
+                        "text-slate-500",
+                        isActive && "text-slate-700",
+                        isCompleted && "text-emerald-700"
+                    )}
+                />
                 {label}
             </div>
+            <div className={cn(
+                "ml-auto opacity-0 border-2 border-slate-700 h-full transition-all",
+                isActive && "opacity-100",
+                isCompleted && "border-emerald-700"
+            )} />
         </button>
     )
 }
