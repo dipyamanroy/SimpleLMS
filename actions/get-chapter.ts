5be0e14fe04176355576f7386f43c3cb7fd26fdc
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-14 18:21:04 +0530

    Added error handling of get-chapter action.

---
+++ actions/get-chapter.ts
@@ -0,0 +1,29 @@
+import { db } from "@/lib/db";
+import { Attachment, Chapter } from "@prisma/client";
+
+interface GetChapterProps{
+    userId: string;
+    courseId: string;
+    chapterId: string;
+}
+
+export const getChapter = async ({
+    userId,
+    courseId,
+    chapterId,
+}: GetChapterProps) => {
+    try {
+        
+    } catch (error) {
+        console.log("[GET_CHAPTER]", error);
+        return {
+            chapter: null,
+            course: null,
+            muxData: null,
+            attachments: [],
+            nextChapter: null,
+            userProgress: null,
+            purchase: null,
+        }
+    }
+}
--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,4 +1,18 @@
-const ChapterIdPage = () => {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const ChapterIdPage = async ({
+    params
+}: {
+    params: { courseId: string; chapterId: string },
+}) => {
+    const { userId } = auth();
+
+    if (!userId) {
+        return redirect("/");
+    }
+
+    
     return ( 
         <div>
             Chapter Id!
