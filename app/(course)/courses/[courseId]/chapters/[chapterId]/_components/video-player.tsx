6bdfc0fd737c287f60b4c3e4fba71eaf4823bf07
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-14 18:57:11 +0530

    Added video playback props for locked chapters.

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -36,6 +36,14 @@ export const VideoPlayer = ({
                     <Loader2 className="h-8 w-8 animate-spin text-secondary"/>
                 </div>
             )}
+            {isLocked && (
+                <div className="absolute inset-0 flex items-center justify-center bg-slate-800 flex-col gap-y-2 text-secondary">
+                    <Lock className="h-8 w-8"/>
+                    <p className="text-sm">
+                        This chapter is locked
+                    </p>
+                </div>
+            )}
         </div>
     )
 }
