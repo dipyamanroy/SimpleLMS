08308dbf421f0087440947c29766c4479728ceb7
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-14 18:24:35 +0530

    Added fetching course, chapter and purchase information from db for get-chapter action.

--- actions/get-chapter.ts
+++ actions/get-chapter.ts
@@ -13,7 +13,36 @@ export const getChapter = async ({
     chapterId,
 }: GetChapterProps) => {
     try {
-        
+        const purchase = await db.purchase.findUnique({
+            where: {
+                userId_courseId: {
+                    userId,
+                    courseId,
+                },
+            },
+        });
+
+        const course = await db.course.findUnique({
+            where: {
+                isPublished: true,
+                id: courseId,
+            },
+            select: {
+                price: true,
+            }
+        });
+
+        const chapter = await db.chapter.findUnique({
+            where: {
+                id: chapterId,
+                isPublished: true,
+            }
+        });
+
+        if (!chapter || !course) {
+            throw new Error("Chapter or course not found");
+        }
+
     } catch (error) {
         console.log("[GET_CHAPTER]", error);
         return {
