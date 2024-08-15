e442df7e9524f712c16a6174db10ef5606f8f965
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-15 18:51:49 +0530

    Added progress from shadcn for use in course progress component.

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -3,6 +3,8 @@ import { Chapter, Course, UserProgress } from "@prisma/client"
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
+import { CourseProgress } from "@/components/course-progress";
+
 import { CourseSidebarItem } from "./course-sidebar-item";
 
 interface CourseSidebarProps{
@@ -39,14 +41,14 @@ export const CourseSidebar = async ({
                 <h1 className="font-semibold">
                     {course.title}
                 </h1>
-                {purchase && (
+                {/* {purchase && ( */}
                     <div className="mt-10">
                         <CourseProgress 
                             variant="success"
                             value={progressCount}
                         />
                     </div>
-                )}
+                {/* )} */}
             </div>
             <div className="flex flex-col w-full">
                 {course.chapters.map((chapter) => (
---
+++ components/course-progress.tsx
@@ -0,0 +1,28 @@
+interface CourseProgressProps {
+    value: number,
+    variant?: "default" | "success",
+    size?: "default" | "sm",
+}
+
+const colorByVariant = {
+    default: "text-sky-700",
+    success: "text-emerald-700", 
+}
+
+const sizeByVariant = {
+    default: "text-sm",
+    sm: "text-xs", 
+}
+
+export const CourseProgress = ({
+    value,
+    variant,
+    size,
+}: CourseProgressProps) => {
+    return (
+        <div>
+            <div>
+            </div>
+        </div>
+    )
+}
