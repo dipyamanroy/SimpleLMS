7fe9340299f62dfc51f094ce0463cd0754b6d49a
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-15 17:40:54 +0530

    Added MuxPlayer placeholder in video-player component.

--- app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/_components/video-player.tsx
@@ -29,9 +29,11 @@ export const VideoPlayer = ({
     isLocked,
     completeOnEnd,
 }: VideoPlayerProps) => {
+    const [isReady, setIsReady] = useState(false);
+
     return (
         <div className="relative aspect-video">
-            {!isLocked && (
+            {!isReady && !isLocked && (
                 <div className="absolute inset-0 flex items-center justify-center bg-slate-800">
                     <Loader2 className="h-8 w-8 animate-spin text-secondary"/>
                 </div>
@@ -43,6 +45,17 @@ export const VideoPlayer = ({
                         This chapter is locked
                     </p>
                 </div>
+            )}
+            {!isLocked && (
+                <MuxPlayer
+                    title={title}
+                    className={cn(
+                        !isReady && "hidden",
+                    )}
+                    onCanPlay={() => setIsReady(true)}
+                    onEnded={() => {}}
+                    autoPlay
+                />
             )}
         </div>
     )
