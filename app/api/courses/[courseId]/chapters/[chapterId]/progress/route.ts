b3c5417807f177744161c98fdb1a6e6077cdccf3
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-17 16:37:50 +0530

    Handled creation of userProgress in db for the first time and updation of the same.

--- app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
+++ app/api/courses/[courseId]/chapters/[chapterId]/progress/route.ts
@@ -1,6 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+import { db } from "@/lib/db";
+
 export async function PUT(
     req: Request,
     { params }: { params: { courseId: string; chapterId: string} }
@@ -12,6 +14,23 @@ export async function PUT(
         if (!userId) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
+
+        const userProgress = await db.userProgress.upsert({
+            where: {
+                userId_chapterId: { 
+                    userId, 
+                    chapterId: params.chapterId,
+                },
+            },
+            update: {
+                isCompleted,
+            },
+            create: {
+                userId,
+                chapterId: params.chapterId,
+                isCompleted,
+            },
+        });
 
     } catch (error) {
         console.log("[CHAPTER_ID_PROGRESS]", error);
