894057d5239af97ef5277b7ccf3b71e78f088464
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-18 16:27:14 +0530

    Added logic for finding progress out of purchased courses.

--- actions/get-dashboard-courses.ts
+++ actions/get-dashboard-courses.ts
@@ -1,6 +1,7 @@
 import { Category, Chapter, Course } from "@prisma/client"
 
 import { db } from "@/lib/db";
+import { getProgress } from "@/actions/get-progress";
 
 type CourseWithProgressWithCategory = Course & {
     category: Category;
@@ -34,6 +35,11 @@ export const getDashboardCourses = async (userId: string): Promise<DashboardCour
         });
 
         const courses = purchasedCourses.map((purchase) => purchase.course) as CourseWithProgressWithCategory[];
+
+        for (let course of courses) {
+            const progress = await getProgress(userId, course.id);
+            course["progress"] = progress;
+        }
 
     } catch (error) {
         console.log("[GET_DASHBOARD_COURCES]", error);
