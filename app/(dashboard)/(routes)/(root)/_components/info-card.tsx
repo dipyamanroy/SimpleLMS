b3c71f927c2a80da20d814700a08dec598f51ae3
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-18 16:52:13 +0530

    Finalized info-card component.

--- app/(dashboard)/(routes)/(root)/_components/info-card.tsx
+++ app/(dashboard)/(routes)/(root)/_components/info-card.tsx
@@ -19,7 +19,16 @@ export const InfoCard  = ({
         <div className="border rounded-md flex items-center gap-x-2 p-3">
             <IconBadge 
                 variant={variant}
+                icon={Icon}
             />
+            <div>
+                <p className="font-medium">
+                    {label}
+                </p>
+                <p className="text-gray-500 text-sm">
+                    {numberOfItems} {numberOfItems === 1 ? "Course" : "Courses"}
+                </p>
+            </div>
         </div>
     )
 }
