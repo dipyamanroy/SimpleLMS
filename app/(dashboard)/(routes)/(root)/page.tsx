5926df1aed8924324cf1a6721e5d6897403393b9
dipyamanroy <d1pyamanroy@protonmail.com>
2024-08-18 16:37:36 +0530

    Added info card boilerplate in student courses dashboard.

--- app/(dashboard)/(routes)/(root)/page.tsx
+++ app/(dashboard)/(routes)/(root)/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs/server"
 import { redirect } from "next/navigation";
 
 import { getDashboardCourses } from "@/actions/get-dashboard-courses";
+import { CoursesList } from "@/components/courses-list";
 
 export default async function Dashboard() {
     const { userId } = auth();
@@ -16,8 +17,18 @@ export default async function Dashboard() {
     } = await getDashboardCourses(userId);
 
     return (
-        <div>
-            
+        <div className="p-6 space-y-4">
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+                <div>
+                    {/* Info card */}
+                </div>
+                <div>
+                    {/* Info card */}
+                </div>
+            </div>
+            <CoursesList 
+                items={[...coursesInProgress, ...completedCourses]}
+            />
         </div>
     )
 }
